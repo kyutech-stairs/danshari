a0ce40456c699622a34be3e901b16061a1022ea2
山口迪夏 <yamaguchifumika@planningdevnomacbook-pro.local>
2018-06-27 18:26:34 +0900

    Initial Commit

---
+++ danshari/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  danshari
+//
+//  Created by 山口迪夏 on 2018/06/27.
+//  Copyright © 2018年 山口迪夏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ danshariTests/danshariTests.swift
@@ -0,0 +1,36 @@
+//
+//  danshariTests.swift
+//  danshariTests
+//
+//  Created by 山口迪夏 on 2018/06/27.
+//  Copyright © 2018年 山口迪夏. All rights reserved.
+//
+
+import XCTest
+@testable import danshari
+
+class danshariTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
