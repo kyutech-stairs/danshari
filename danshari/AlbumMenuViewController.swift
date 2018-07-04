61b3e23aebd9f02a7eca6a9a04b7307c5ba603d0
Hikaru Akiyama <l2f1.6003.akiyama@gmail.com>
2018-07-04 18:14:51 +0900

    fix

---
+++ danshari/AlbumMenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AlbumoMenuViewController.swift
+//  danshari
+//
+//  Created by 山口迪夏 on 2018/07/02.
+//  Copyright © 2018年 山口迪夏. All rights reserved.
+//
+
+import UIKit
+
+class AlbumMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
